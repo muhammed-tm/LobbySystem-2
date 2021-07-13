146fd0c0f49cb85964c242dc3e99f2e2b50f237a
QuadrixYT <andrehillay@gmail.com>
2021-07-13 23:35:46 +0200

    Fix Item Move

--- src/main/java/eu/hypetime/spigot/hypelobby/listener/MainListener.java
+++ src/main/java/eu/hypetime/spigot/hypelobby/listener/MainListener.java
@@ -1,9 +1,13 @@
 package eu.hypetime.spigot.hypelobby.listener;
 
+import eu.hypetime.spigot.hypelobby.commands.BuildCommand;
 import io.papermc.paper.event.entity.EntityMoveEvent;
+import org.bukkit.GameMode;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.potion.PotionEffect;
@@ -28,6 +32,13 @@ public class MainListener implements Listener {
      @EventHandler
      public void onMove(EntityMoveEvent event) {
           if(event.getEntityType() == EntityType.ARMOR_STAND) {
+               event.setCancelled(true);
+          }
+     }
+
+     @EventHandler
+     public void onItemMove(InventoryMoveItemEvent event) {
+          if(!BuildCommand.build.contains((Player) event.getSource().getViewers().get(0))) {
                event.setCancelled(true);
           }
      }
