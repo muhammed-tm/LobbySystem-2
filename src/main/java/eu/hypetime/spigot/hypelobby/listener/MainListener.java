46799b4e5d66b8f52938ca03dd534f74f5e16805
QuadrixYT <andrehillay@gmail.com>
2021-07-18 18:57:42 +0200

    Disable Item Move

--- src/main/java/eu/hypetime/spigot/hypelobby/listener/MainListener.java
+++ src/main/java/eu/hypetime/spigot/hypelobby/listener/MainListener.java
@@ -2,12 +2,12 @@ package eu.hypetime.spigot.hypelobby.listener;
 
 import eu.hypetime.spigot.hypelobby.commands.BuildCommand;
 import io.papermc.paper.event.entity.EntityMoveEvent;
-import org.bukkit.GameMode;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.potion.PotionEffect;
@@ -22,7 +22,7 @@ public class MainListener implements Listener {
 
      @EventHandler
      public void onSneak(PlayerToggleSneakEvent event) {
-          if(!event.getPlayer().isSneaking()) {
+          if (!event.getPlayer().isSneaking()) {
                event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 99999, 1));
           } else {
                event.getPlayer().removePotionEffect(PotionEffectType.INVISIBILITY);
@@ -31,15 +31,15 @@ public class MainListener implements Listener {
 
      @EventHandler
      public void onMove(EntityMoveEvent event) {
-          if(event.getEntityType() == EntityType.ARMOR_STAND) {
+          if (event.getEntityType() == EntityType.ARMOR_STAND) {
                event.setCancelled(true);
           }
      }
 
      @EventHandler
-     public void onItemMove(InventoryMoveItemEvent event) {
-          if(!BuildCommand.build.contains((Player) event.getSource().getViewers().get(0))) {
-               event.setCancelled(true);
+     public void onClick(InventoryClickEvent event) {
+          if (event.getInventory().getType() == InventoryType.PLAYER) {
+               event.setCancelled(!BuildCommand.build.contains((Player) event.getWhoClicked()));
           }
      }
 
