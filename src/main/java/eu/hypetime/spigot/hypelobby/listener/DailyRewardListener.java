d52b59b9d273ec0df02c2bfba3eee65a6aac795c
QuadrixYT <andrehillay@gmail.com>
2021-08-24 11:07:27 +0200

    Add Stats message when Rightclick ArmorStand of Top 3

--- src/main/java/eu/hypetime/spigot/hypelobby/listener/DailyRewardListener.java
+++ src/main/java/eu/hypetime/spigot/hypelobby/listener/DailyRewardListener.java
@@ -1,6 +1,8 @@
 package eu.hypetime.spigot.hypelobby.listener;
 
 import eu.hypetime.spigot.hypelobby.utils.Inventories;
+import eu.hypetime.spigot.hypelobby.utils.SQLStats;
+import eu.hypetime.spigot.hypelobby.utils.StatsManager;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
@@ -16,11 +18,20 @@ public class DailyRewardListener implements Listener {
     @EventHandler
     public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event) {
         if (event.getRightClicked().getType() == EntityType.ARMOR_STAND) {
+            Player player =event.getPlayer();
             ArmorStand armorStand = (ArmorStand) event.getRightClicked();
             if (armorStand.getType().equals(EntityType.ARMOR_STAND)) {
                 if (event.getRightClicked().getCustomName() != null) {
                     if (event.getRightClicked().getCustomName().equalsIgnoreCase("§6Daily Rewards")) {
                         Inventories.DailyRewardInventory(event.getPlayer());
+                    } else if(event.getRightClicked().getCustomName().contains("§6")) {
+                        String target = event.getRightClicked().getCustomName().replace("§6", "");
+                        player.sendMessage("§8§m          §8[§aStats§8]§8§m          ");
+                        player.sendMessage("§7Spieler§8: §a" + target);
+                        player.sendMessage("§7Kills§8: §a" + StatsManager.getKills(target));
+                        player.sendMessage("§7Deaths§8: §a" + StatsManager.getDeaths(target));
+                        player.sendMessage("§7Rank§8: §a" + SQLStats.getInstance().getRanking(target));
+                        player.sendMessage("§8§m          §8[§aStats§8]§8§m          ");
                     }
                     event.setCancelled(true);
                 }
