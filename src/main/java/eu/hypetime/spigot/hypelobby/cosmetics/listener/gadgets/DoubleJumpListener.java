6070a66ac7e2bf93462ab2e0e1ae03632b9a1ba4
QuadrixYT <andrehillay@gmail.com>
2022-02-27 00:49:34 +0100

    Fix Double Jump

--- src/main/java/eu/hypetime/spigot/hypelobby/cosmetics/listener/gadgets/DoubleJumpListener.java
+++ src/main/java/eu/hypetime/spigot/hypelobby/cosmetics/listener/gadgets/DoubleJumpListener.java
@@ -2,12 +2,10 @@ package eu.hypetime.spigot.hypelobby.cosmetics.listener.gadgets;
 
 import eu.hypetime.spigot.hypelobby.utils.SettingConfig;
 import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
 
 
@@ -16,16 +14,16 @@ public class DoubleJumpListener implements Listener {
      @EventHandler
      public void onFly(PlayerToggleFlightEvent event) {
           Player player = event.getPlayer();
-               if (player.getGameMode() == GameMode.SURVIVAL) {
-                    if(SettingConfig.getDoublejump(player) == false) {
-                         event.setCancelled(true);
-                    player.setAllowFlight(false);
-                    player.setFlying(false);
-                    player.setVelocity(player.getLocation().getDirection().setY(0.65).multiply(1.9));
-
-
-                    player.playSound(player.getLocation(), Sound.ENTITY_WITHER_SHOOT, 1, 2F);
+          if (player.getGameMode() == GameMode.SURVIVAL) {
+               if (SettingConfig.getDoublejump(player) == false) {
+                    event.setCancelled(true);
                }
+               player.setAllowFlight(false);
+               player.setFlying(false);
+               player.setVelocity(player.getLocation().getDirection().setY(0.65).multiply(1.9));
+
+
+               player.playSound(player.getLocation(), Sound.ENTITY_WITHER_SHOOT, 1, 2F);
           }
      }
 }
